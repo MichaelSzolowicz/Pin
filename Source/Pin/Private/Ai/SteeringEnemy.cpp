e8110f893608a8591d719be3e0d86d8001d5be36
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2023-12-24 16:55:03 -0800

    Debug lines

--- Source/Pin/Private/Ai/SteeringEnemy.cpp
+++ Source/Pin/Private/Ai/SteeringEnemy.cpp
@@ -25,16 +25,23 @@ void ASteeringEnemy::Tick(float DeltaTime)
 
 	FVector Input = Steering->GetInput();
 	Input.Normalize();
+	//Input.Z = 0.0f;
 
 	// Try to move against velocity if we are speeding
 	if (Movement->GetComponentVelocity().Size() > Movement->GetMaxSpeed() + 0.1f) {
-		// * 1.1 so counter velocity input is always larger than base input.
-		Input += -Movement->GetComponentVelocity().GetSafeNormal() * 1.1f;
+		// * 1.05 so counter velocity input is always larger than base input.
+		Input += -Movement->GetComponentVelocity().GetSafeNormal() * 1.05f;
 		Input.Normalize();
 	}
 
+
+
 	Movement->AddInputDirection(Input);
 	Movement->Move(DeltaTime);
+
+	Input.Z = 0;
+	DrawDebugLine(GetWorld(), GetActorLocation(),
+		GetActorLocation() + Input * 200, FColor::Magenta, false, 1.0f);
 }
 
 void ASteeringEnemy::Move()
